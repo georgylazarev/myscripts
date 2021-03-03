92fa9e857d65311df9c883fd617a82a07cb5d421
georgylazarev <geolaz@gmail.com>
2021-03-03 13:17:20 +0300

    Update restoreMailbox.sh

--- restoreMailbox.sh
+++ restoreMailbox.sh
@@ -5,6 +5,6 @@ zmprov=/opt/zimbra/bin/zmprov
 
 backup=$2 # Путь до архива, включая название файла 
 mbox=$1 # Ящик, куда производится восстановление
-echo "$(date +%T): Begin restoring files from $mbox";
+echo "$(date +%T): Начало восстановления писем для $mbox";
 $zmbox -z -m $mbox -t 0 postRestURL "//?fmt=tgz&resolve=skip" $backup
-echo "$(date +%T): End restoring files from $mbox";
+echo "$(date +%T): Окончание восстановления писем для $mbox";
