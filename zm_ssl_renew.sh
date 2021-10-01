93f70393015168c1e501e6dbe8709a4b3041f282
George Lazarev <geolaz@gmail.com>
2021-10-01 21:55:17 +0300

    Update zm_ssl_renew.sh
    certbot and new root ca

--- zm_ssl_renew.sh
+++ zm_ssl_renew.sh
@@ -1,12 +1,12 @@
 su - zimbra -c "/opt/zimbra/bin/zmproxyctl stop"
 su - zimbra -c "/opt/zimbra/bin/zmmailboxdctl stop"
-/opt/certbot/certbot-auto renew --standalone
+certbot renew --standalone
 rm -f /opt/zimbra/ssl/letsencrypt-old/*
 cp /opt/zimbra/ssl/letsencrypt/* /opt/zimbra/ssl/letsencrypt-old/
 rm -f /opt/zimbra/ssl/letsencrypt/* 
 rm -f /opt/zimbra/ssl/zimbra/commercial/commercial.key 
 cp /etc/letsencrypt/live/mail.an-security.ru/* /opt/zimbra/ssl/letsencrypt/
-cat /opt/certbot/letsencryptCA >> /opt/zimbra/ssl/letsencrypt/chain.pem
+cat /opt/myscripts/letsencryptCA >> /opt/zimbra/ssl/letsencrypt/chain.pem
 cp /opt/zimbra/ssl/letsencrypt/privkey.pem /opt/zimbra/ssl/zimbra/commercial/commercial.key
 chown zimbra:zimbra /opt/zimbra/ssl/letsencrypt/*
 chown zimbra:zimbra /opt/zimbra/ssl/zimbra/commercial/commercial.key
