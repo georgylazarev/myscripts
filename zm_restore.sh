0c43a687c84ab55a660e5ac77b745ebc629e40be
George Lazarev <geolaz@gmail.com>
2022-01-18 14:25:55 +0300

    Update zm_restore.sh

--- zm_restore.sh
+++ zm_restore.sh
@@ -10,14 +10,14 @@ restoreDay=$5;
 zmbox="/opt/zimbra/bin/zmmailbox";
 
 case $period in
-  [Yy] ) backupFolder="/opt/zimbra/backup/backups/$mbox/$restoreYear"; break;;
-  [Mm] ) backupFolder="/opt/zimbra/backup/backups/$mbox/$restoreYear/$restoreMonth"; break;;
-  [Dd] ) backupFolder="/opt/zimbra/backup/backups/$mbox/$restoreYear/$restoreMonth/$restoreDay"; break;;
-  * ) echo "Введите период восстановления Y - год, M - месяц, D - день"; exit;;
+  [Yy] ) backupFolder="/opt/zimbra/backup/backups/$mbox/$restoreYear";;
+  [Mm] ) backupFolder="/opt/zimbra/backup/backups/$mbox/$restoreYear/$restoreMonth";;
+  [Dd] ) backupFolder="/opt/zimbra/backup/backups/$mbox/$restoreYear/$restoreMonth/$restoreDay";;
+  * ) echo "Введите период восстановления Y - год, M - месяц, D - день";;
 esac
 
 # Скрипт рекурсивно просматривает указанные директории
 for file in `find $backupFolder -type f -name "*.tgz"`
 do
-  $zmbox -z -m $mbox -t 0 postRestURL "//?fmt=tgz&resolve=skip" $backupFolder/$file;
+  $zmbox -z -m $mbox -t 0 postRestURL "//?fmt=tgz&resolve=skip" $file;
 done
