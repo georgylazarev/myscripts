e8c4e9a17b6e5f2f890d127d20ad1e07fd8110b2
George Lazarev <geolaz@gmail.com>
2022-01-18 14:17:33 +0300

    Update zm_restore.sh

--- zm_restore.sh
+++ zm_restore.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+period=$1; # Задается период восстановления - год, месяц или день
+mbox=$2; # Электронная почта сотрудника
+
+restoreYear=$3;
+restoreMonth=$4;
+restoreDay=$5;
+
+zmbox="/opt/zimbra/bin/zmmailbox";
+
+case $period in
+  [Yy] ) backupFolder="/opt/zimbra/backup/backups/$mbox/$restoreYear"; break;;
+  [Mm] ) backupFolder="/opt/zimbra/backup/backups/$mbox/$restoreYear/$restoreMonth"; break;;
+  [Dd] ) backupFolder="/opt/zimbra/backup/backups/$mbox/$restoreYear/$restoreMonth/$restoreDay"; break;;
+  * ) echo "Введите период восстановления Y - год, M - месяц, D - день"; exit;;
+esac
+
+# Скрипт рекурсивно просматривает указанные директории
+for file in `find $backupFolder -type f -name "*.tgz"`
+do
+  $zmbox -z -m $mbox -t 0 postRestURL "//?fmt=tgz&resolve=skip" $backupFolder/$file;
+done
